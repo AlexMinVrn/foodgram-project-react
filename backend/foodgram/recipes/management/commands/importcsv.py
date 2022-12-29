9ecba6c2725358eb89a7c769287f282dfbf927f8
Александр Минаев <a_minaev14@mail.ru>
2022-12-29 20:50:06 +0300

    Поправил importcsv.py.

--- backend/foodgram/recipes/management/commands/importcsv.py
+++ backend/foodgram/recipes/management/commands/importcsv.py
@@ -1,20 +1,19 @@
 import csv
-import sqlite3
+import os
 
 from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredients
 
 
 class Command(BaseCommand):
     """Обработчик менеджмент-команды по импорту csv-данных в БД SQLite."""
     def handle(self, *args, **options):
-        connection = sqlite3.connect('db.sqlite3')
-        cursor = connection.cursor()
-
-        with open('data/ingredients.csv', 'r', encoding='utf8') as fin:
+        Ingredients.objects.all().delete()
+        with open(os.path.join("data", "ingredients.csv"), 'r', encoding='utf-8') as fin:
             dr = csv.DictReader(fin)
-            to_db = [(i['name'], i['measurement_unit']) for i in dr]
-
-        cursor.executemany(
-            'INSERT INTO recipes_ingredients (name, measurement_unit) VALUES (?, ?);', to_db)
-        connection.commit()
-        connection.close()
+            for i in dr:
+                Ingredients.objects.get_or_create(
+                    name=i['name'],
+                    measurement_unit=i['measurement_unit']
+                )
