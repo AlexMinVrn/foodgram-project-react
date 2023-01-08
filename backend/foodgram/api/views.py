ad92d9fc8c77fea2d45bdc0a9211fe7f61e570ee
Александр Минаев <a_minaev14@mail.ru>
2023-01-08 22:18:02 +0300

    Поправил ToSubscribeView.

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -44,7 +44,7 @@ class ToSubscribeView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
         following = get_object_or_404(User, id=id)
